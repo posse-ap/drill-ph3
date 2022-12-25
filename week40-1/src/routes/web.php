a24b8af18c8c5d6a185318841c81d38374f0123e
manaki-e <48690114s@gmail.com>
2022-12-25 11:10:08 +0900

    week40-1終了

--- week40-1/src/routes/web.php
+++ week40-1/src/routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index');
 
-Route::get('/login', 'Auth\LoginController@login');
+Route::get('/login', 'Auth\LoginController@showLoginForm');
 
 
